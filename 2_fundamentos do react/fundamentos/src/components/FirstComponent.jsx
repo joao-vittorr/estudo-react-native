74d25617e8a65291365b8b1ff2b0fe584053d635
João Vitor Alexandre Ribeiro <vitorribeiro9454@gmail.com>
2025-02-11 16:36:36 -0300

    Primeiro componente

---
+++ 2_fundamentos do react/fundamentos/src/components/FirstComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FirstComponent = () => {
+    return (
+        <div>
+            <h2>This is my first React component!</h2>
+        </div>
+    );
+};
+
+export default FirstComponent;
