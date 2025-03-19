f97dcb3dddd4bef86b30d36a24714ecdd232d649
João Vitor Alexandre Ribeiro <vitorribeiro9454@gmail.com>
2025-03-19 15:46:39 -0300

    Criando reaproveitamento do componente com loop map

--- 3_avancando_em_react/avancando/src/App.jsx
+++ 3_avancando_em_react/avancando/src/App.jsx
@@ -7,6 +7,12 @@ import ShowUserName from "./components/ShowUserName";
 import CarDetails from "./components/CarDetails";
 
 function App() {
+  const carros = [
+    { marca: "VW", modelo: "Gol", ano: 2008, newCar: false },
+    { marca: "Fiat", modelo: "Palio", ano: 2010, newCar: false },
+    { marca: "Chevrolet", modelo: "Onix", ano: 2025, newCar: true },
+  ];
+
   return (
     <>
       <h1>Conhecimentos avançando em React</h1>
@@ -21,6 +27,12 @@ function App() {
         {/* Reaproveitando*/}
         <CarDetails marca="Fiat" modelo="Palio" ano={2010} newCar={false}> </CarDetails>
         <CarDetails marca="Chevrolet" modelo="Onix" ano={2025} newCar={true}> </CarDetails>
+        <br /><hr />
+        <h2>Reaproveitando com map</h2>
+        {/* Reaproveitando com map */}
+        {carros.map((carro) => (
+          <CarDetails marca={carro.marca} modelo={carro.modelo} ano={carro.ano} newCar={carro.newCar}> </CarDetails>
+        ))}
       </div>
     </>
   );
