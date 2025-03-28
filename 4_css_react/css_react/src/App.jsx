ffb3b6ceddbd2d1ae67623574043bcb5b95ca340
João Vitor Alexandre Ribeiro <vitorribeiro9454@gmail.com>
2025-03-27 21:04:56 -0300

    Adicionando css react e iniciando pratica de css global e componente

---
+++ 4_css_react/css_react/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Mycomponent from "./components/Mycomponent";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="App">
+        {/*Css Global */}
+        <h1>React CSS</h1>
+        {/*Css de componente */}
+        <Mycomponent />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ 4_css_react/css_react/src/components/Mycomponent.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Mycomponent.css";
+
+const Mycomponent = () => {
+  return (
+    <div>
+      <h1>Meu Css de componente</h1>
+    </div>
+  );
+};
+
+export default Mycomponent;
