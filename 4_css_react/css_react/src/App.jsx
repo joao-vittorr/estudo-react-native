f27e499c644813cf2544a342e8aa34f8afd7ad33
João Vitor Alexandre Ribeiro <vitorribeiro9454@gmail.com>
2025-04-04 16:19:28 -0300

    Css inline dinamico, css modules e classe dinanmica

--- 4_css_react/css_react/src/App.jsx
+++ 4_css_react/css_react/src/App.jsx
@@ -3,9 +3,12 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Mycomponent from "./components/Mycomponent";
+import Title from "./components/Title";
 
 function App() {
   const [count, setCount] = useState(0);
+  const n = 2;
+  const redTitle = false;
 
   return (
     <>
@@ -17,6 +20,14 @@ function App() {
         <p>Esse paragrado é do App.js</p>
         {/*Css inline */}
         <p style={{color:"black", backgroundColor:"white", padding: "25px", borderTop: "5px solid red"}}>Esse Elemento foi estilizado usando css inline</p>
+        {/*Css inline dinâmico*/}
+        <p style={{color: n > 5 ? "green" : "red"}}>Esse Elemento foi estilizado usando css inline dinâmico</p>
+        <p style={{color: n < 5 ? "green" : "red"}}>Esse Elemento foi estilizado usando css inline dinâmico</p>
+        {/* Classe dinâmica */}
+        <h2 className= {redTitle ? "red-title" : "title" }>Este título terá classe dinâmica</h2>
+        {/* css modules */}
+        <Title/>
+        <h2 className="my_title">Testando se o my-title vasa</h2>
       </div>
     </>
   );
