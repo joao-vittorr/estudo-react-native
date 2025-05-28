4522254569a8af2aead40bd2a0ad68a2bf5c04ff
João Vitor Alexandre Ribeiro <vitorribeiro9454@gmail.com>
2025-05-28 16:23:46 -0300

    Organizando App jsx e criando os components

--- 6_secret_word/secret_word/src/App.jsx
+++ 6_secret_word/secret_word/src/App.jsx
@@ -1,14 +1,32 @@
 import { useState } from "react";
 import "./App.css";
-import StartScreen from "./components/StartScreen";
 
+import { useCallback, useEffec, useState } from "react";
+
+// import data
+import {wordsList}from "./data/words";
+
+// import components
+import StartScreen from "./components/StartScreen";
+import GameScreen from "./components/GameScreen";
+import EndScreen from "./components/EndScreen";
+
+const stages = [
+  { id: 1, name: "start" },
+  { id: 2, name: "game" },
+  { id: 3, name: "end" },
+]
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [gameStage, setGameStage] = useState(stages[0].name);
+  const [secretWord, setSecretWord] = useState("");
 
   return (
     <>
-      <StartScreen/>
+      {gameStage === "start" && <StartScreen/>}
+      {gameStage === "game" && <GameScreen secretWord={secretWord} setGameStage={setGameStage} />}
+      {gameStage === "end" && <EndScreen secretWord={secretWord} setGameStage={setGameStage} />}  
+
     </>
   );
 }
