1ff0550fc27b252f196feced15b194ae770ade97
João Vitor Alexandre Ribeiro <vitorribeiro9454@gmail.com>
2025-05-28 15:58:59 -0300

    Ajustando App css

--- 6_secret_word/secret_word/src/App.jsx
+++ 6_secret_word/secret_word/src/App.jsx
@@ -1,14 +1,14 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <h2>Secret Word</h2>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
