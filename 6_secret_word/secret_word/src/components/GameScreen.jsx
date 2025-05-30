d93f24ba3c1ec0c4d66ba54ccd16d5c0ba3d1cf5
João Vitor Alexandre Ribeiro <vitorribeiro9454@gmail.com>
2025-05-30 17:43:25 -0300

    Criando a tela do game, faltando a estilização

--- 6_secret_word/secret_word/src/components/GameScreen.jsx
+++ 6_secret_word/secret_word/src/components/GameScreen.jsx
@@ -3,9 +3,34 @@ import "./GameScreen.css";
 
 const GameScreen = ({ verifyLetter }) => {
   return (
-    <div>
-      <h1>Game</h1>
-      <button onClick={verifyLetter}>Finalizar jogo</button>
+    <div className="game-screen">
+      <p className="points">
+        <span>Pontuação: </span>
+      </p>
+      <h1>Advinhe a palavra:</h1>
+      <h3 className="tip">
+        Dica sobre a palavra: <span> </span>
+      </h3>
+      <div className="wordContainer">
+        <span className="letter">A</span>
+        <span className="blankSquare"></span>
+      </div>
+      <div className="letterContainer">
+        <p>Tente advinhar a palabvra:</p>
+        <form onSubmit={(e) => e.preventDefault()}>
+          <input
+            type="text"
+            maxLength="1"
+            required
+            onChange={(e) => console.log(e.target.value)}
+          />
+          <button onClick={verifyLetter}>Verificar</button>
+        </form>
+      </div>
+      <div className="wrongLettersContainer">
+        <p>Letras já utilizadas:</p>
+        <span className="wrongLetter">B</span>
+      </div>
     </div>
   );
 };
