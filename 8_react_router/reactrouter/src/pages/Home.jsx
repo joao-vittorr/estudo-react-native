c3d77ab4977fa745014726e218bc70d67da9e746
João Vitor <vitorribeiro9454@gmail.com>
2025-09-04 19:12:14 -0300

    Apresentando dados do bd e estilizando

--- 8_react_router/reactrouter/src/pages/Home.jsx
+++ 8_react_router/reactrouter/src/pages/Home.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import { useFetch } from "../hooks/useFetch";
+
+import "./Home.css";
 
 const Home = () => {
+  // carregamento de dados
+  const url = "http://localhost:3000/products";
+  const { data: items, loading, error } = useFetch(url);
+
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      <h1>Produtos</h1>
+      {loading && <p>Loading...</p>}
+      {error && <p>Error: {error}</p>}
+      <ul className="product-list">
+        {items &&
+          items.map((item) => (
+            <li key={item.id}>
+              <h2>{item.name}</h2>
+              <p>{item.description}</p>
+              <Link to={`/products/${item.id}`}>View Details</Link>
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
+};
 
 export default Home;
