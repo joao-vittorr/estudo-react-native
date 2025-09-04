77787a6a448db5d00f21b1e4d471412254c5aa2c
João Vitor <vitorribeiro9454@gmail.com>
2025-09-04 17:24:06 -0300

    Criando página home e about e configurando rotas, criando component navbar

--- 8_react_router/reactrouter/src/App.jsx
+++ 8_react_router/reactrouter/src/App.jsx
@@ -1,33 +1,26 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+// 1 - Configurar o React Router
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+
+// 2 - import das páginas
+import Home from './pages/Home'
+import About from './pages/About'
+
+// 3 import dos componentes
+import Navbar from './components/Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>React Router</h1>
+      <Navbar />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
---
+++ 8_react_router/reactrouter/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <a href="/">Home</a>
+      <a href="/about">About</a>
+    </nav>
+  )
+}
+
+export default Navbar;
