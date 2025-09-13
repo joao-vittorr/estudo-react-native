ace2147902b724446142703604359eb181d4a5fc
João Vitor <vitorribeiro9454@gmail.com>
2025-09-13 12:26:26 -0300

    adicionando navlink com classe active

--- 8_react_router/reactrouter/src/components/Navbar.jsx
+++ 8_react_router/reactrouter/src/components/Navbar.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import './Navbar.css'
+import { Link, NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <a href="/">Home</a>
-      <a href="/about">About</a>
+      <NavLink to="/" className={({ isActive }) => (isActive ? 'active' : '')}>Home</NavLink>
+      <NavLink to="/about" className={({ isActive }) => (isActive ? 'active' : '')}>About</NavLink>
     </nav>
   )
 }
